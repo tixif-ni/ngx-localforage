b8e2545dad17493da496597d6b4ff3968f1a79d1
Chris Pawlukiewicz <hi@paynoattn.com>
2017-09-13 16:32:17 -0600

    switched to injectiontoken as opaquetoken was deprecated

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,5 +1,5 @@
-import { OpaqueToken } from '@angular/core';
+import { InjectionToken } from '@angular/core';
 
 export function LocalForageToken() {
-  return new OpaqueToken('localforage');
+  return new InjectionToken('localforage');
 }
