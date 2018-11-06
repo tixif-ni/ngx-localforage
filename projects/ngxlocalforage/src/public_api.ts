86b38d2bcabfccc26c5a08f17c9a40f0dd7ced4c
thduttonuk <tom.dutton@softools.net>
2018-11-06 11:15:12 +0000

    Migrated to a Angular 6 lib.

---
+++ projects/ngxlocalforage/src/public_api.ts
@@ -0,0 +1,5 @@
+export { LocalForageConfiguration } from './lib/configuration.interface';
+export { LocalForageToken } from './lib/localforage.token';
+export { localforageFactory } from './lib/localforage.factory';
+export { LocalForageModule } from './lib/localforage.module';
+export { LocalForageService } from './lib/localforage.service';
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { LocalForageConfiguration } from './configuration.interface';
-export { LocalForageToken } from './localforage.token';
-export { localforageFactory } from './localforage.factory';
-export { LocalForageModule } from './localforage.module';
-export { LocalForageService } from './localforage.service';
